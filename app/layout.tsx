5a226d221d0482e36152353a491ec6dd2384788b
nukeeshock <c8wj97sbt5@privaterelay.appleid.com>
2025-11-29 13:29:08 +0100

    Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,19 +13,19 @@ export const metadata: Metadata = {
   manifest: '/manifest.json',
   icons: {
     icon: [
-      { url: '/karriereadler-favicon.png', type: 'image/png', sizes: '512x512' },
+      { url: '/favicon.png', type: 'image/png', sizes: '512x512' },
       { url: '/favicon.png', type: 'image/png', sizes: '192x192' },
       { url: '/favicon.ico', sizes: '48x48' }
     ],
     apple: [
-      { url: '/karriereadler-favicon.png', sizes: '180x180', type: 'image/png' },
-      { url: '/karriereadler-favicon.png', sizes: '167x167', type: 'image/png' },
-      { url: '/karriereadler-favicon.png', sizes: '152x152', type: 'image/png' },
-      { url: '/karriereadler-favicon.png', sizes: '120x120', type: 'image/png' }
+      { url: '/favicon.png', sizes: '180x180', type: 'image/png' },
+      { url: '/favicon.png', sizes: '167x167', type: 'image/png' },
+      { url: '/favicon.png', sizes: '152x152', type: 'image/png' },
+      { url: '/favicon.png', sizes: '120x120', type: 'image/png' }
     ],
-    shortcut: '/karriereadler-favicon.png',
+    shortcut: '/favicon.png',
     other: [
-      { rel: 'apple-touch-icon-precomposed', url: '/karriereadler-favicon.png' }
+      { rel: 'apple-touch-icon-precomposed', url: '/favicon.png' }
     ]
   },
   appleWebApp: {
